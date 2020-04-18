56467792f4b34334cd888ba6d81154113b3d7b81
Morel Serme <sermemerlin@gmail.com>
2020-04-18 02:28:42 +0000

    Finalisation de l'authentification et traduction

---
+++ resources/lang/fr/home.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | HomeLanguage Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple Home links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'You are logged in! ' => 'Vous etes connecté',
+
+];
